971105888774a393b4be292a86e243ad834529d3
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-06-15 15:06:44 +0300

    INIT

---
+++ app.js
@@ -0,0 +1,23 @@
+// Імпортуємо з socket.io його функцію Server
+const { Server } = require('socket.io');
+
+// socket.io вимагає створення свого web-socket серверу на основі http. Тобто треба створити свій http-server. Але використовувати для цього пакет express немає сенсу, бо нам не потрібні ніякі роути. Нам потрібен чистий http-server
+// Тому простіше імпортувати ф-ю createServer з вбудованого у JS пакету http. Для створення роуту він не підходить, але тут як раз те, що треба.
+const { createServer } = require('http');
+
+// Тепер створюємо пустий http-server:
+const httpServer = createServer();
+
+// Тепер можемо створити io-web-socket server (зазвичай називають просто io):
+// підключити io web-socket server на основі httpServer
+const ioWebSocketServer = new Server(httpServer, {
+  cors: { origin: '*' }, // дозволяє підключення з інших портів (тому що в нас фронтенд буде пов'язаний з бекендом)
+});
+
+// Так само, як і у ws-пакеті підключаємо слухача:
+ioWebSocketServer.on('connection', socket => {
+  console.log('New frontend connected');
+});
+
+// Але через те, що використовуємо http-сервер, то треба його запустити:
+httpServer.listen(3001);
